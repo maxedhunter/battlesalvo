31af708eaf07368d785def5d751a8b50b0b8140b
maxedhunter <maxwelljhe@gmail.com>
2023-06-10 19:36:50 -0400

    Created CHANGELOG.md, updated driver.

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+Changes to PA03 for PA04: 
+- restructuring of rows and columns into x and y, and formatting 
+to the regular coordinate format starting from the top left corner
+- small change of GameResult enum from "WON" to "WIN" 
+- implementing Appendable + Readable into controller, updated
+the relevant tests too
--- src/main/java/cs3500/pa04/Driver.java
+++ src/main/java/cs3500/pa04/Driver.java
@@ -1,21 +1,12 @@
 package cs3500.pa04;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import cs3500.pa03.controller.GamePlay;
 import cs3500.pa03.model.Ai;
-import cs3500.pa03.model.Coord;
 import cs3500.pa03.model.Player;
-import cs3500.pa03.model.Ship;
-import cs3500.pa03.model.ShipType;
 import cs3500.pa04.controller.ProxyController;
-import cs3500.pa04.json.ShipAdapter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * This is the main driver of this project.
@@ -25,17 +16,30 @@ public class Driver {
    * Project entry point6
    *
    * @param args - no command line args required
-   */ //TODO remove exception
-  public static void main(String[] args) throws IOException {
-      Appendable output = System.out;
-      Readable input = new InputStreamReader(System.in);
-      GamePlay gp = new GamePlay(input, output);
+   */
+  public static void main(String[] args) {
+    Appendable output = System.out;
+    Readable input = new InputStreamReader(System.in);
+    GamePlay gp = new GamePlay(input, output);
     if (args.length == 2) {
       String host = args[0];
       int port = Integer.parseInt(args[1]);
-      Socket server = new Socket(host, port);
+      Socket server;
+
+      try {
+        server = new Socket(host, port);
+      } catch (IOException e) {
+        throw new RuntimeException("Unable to create socket.");
+      }
+
       Player ai = new Ai(gp);
-      ProxyController proxyController = new ProxyController(server, ai);
+
+      ProxyController proxyController;
+      try {
+        proxyController = new ProxyController(server, ai);
+      } catch (IOException e) {
+        throw new RuntimeException("Unable to create Proxy Controller.");
+      }
       proxyController.run();
     } else if (args.length == 0) {
       gp.startGame();
